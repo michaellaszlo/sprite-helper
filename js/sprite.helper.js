84fd1d2129dfd063ec2cc8bcfa029c4957915d64
Michael Laszlo <git@michaellaszlo.com>
2014-12-12 09:25:54 -0500

    Draw image in center with the help of jQuery

--- js/sprite.helper.js
+++ js/sprite.helper.js
@@ -1,29 +1,48 @@
 var SpriteHelper = {
-	canvasWidth: 960, canvasHeight: 720,
-  imageSource: '../sprites.png'
+  imageSource: 'sprites.png'
 };
 
-SpriteHelper.zoom = function() {
-	var global = SpriteHelper;
-  global.drawImage(global.image, 0, 0);
+SpriteHelper.reset = function() {
+	var global = SpriteHelper,
+      canvas = global.canvas,
+      context = canvas.context,
+      image = global.image;
+  // Width
+  var viewWidth = Math.min(canvas.width, image.width),
+      targetWidth = Math.min(viewWidth, canvas.width),
+      viewX = (image.width - viewWidth) / 2,
+      targetX = Math.max(0, (canvas.width - viewWidth) / 2);
+  // Height
+  var viewHeight = Math.min(canvas.height, image.height),
+      targetHeight = Math.min(viewHeight, canvas.height),
+      viewY = (image.height - viewHeight) / 2,
+      targetY = Math.max(0, (canvas.height - viewHeight) / 2);
+  // Background
+  context.fillStyle = '#888';
+  context.fillRect(targetX, targetY, targetWidth, targetHeight);
+  context.drawImage(global.image,
+      viewX, viewY, viewWidth, viewHeight,
+      targetX, targetY, targetWidth, targetHeight);
 };
 
 SpriteHelper.load = function () {
 	var global = SpriteHelper;
 
 	var canvas = global.canvas = document.getElementById('mainCanvas');
-	canvas.width = global.canvasWidth;
-	canvas.height = global.canvasHeight;
-	var canvasContainer = document.getElementById('mainCanvasContainer');
-	global.canvasContainer = canvasContainer;
-	canvasContainer.style.width = global.canvasWidth + 'px';
-	canvasContainer.style.height = global.canvasHeight + 'px';
-	var context = canvas.context = canvas.getContext('2d');
+  canvas.context = canvas.getContext('2d');
+  canvas.width = $(window).width();
+  canvas.height = $(window).height();
 
   global.image = new Image();
   global.image.src = global.imageSource;
-  global.zoomLevel = 1;
-  global.zoom();
+  global.image.onload = function () {
+    global.reset();
+    $(window).on('resize', function () {
+      canvas.width = $(window).width();
+      canvas.height = $(window).height();
+      global.reset();
+    });
+  };
 };
 
 window.onload = SpriteHelper.load;
