f1a78ecbdb2d70ebfe989a1a39635558e39d1f79
Michael Laszlo <git@michaellaszlo.com>
2014-12-13 09:06:02 -0500

    Better zooming and bounded panning

--- js/sprite.helper.js
+++ js/sprite.helper.js
@@ -4,85 +4,148 @@ var SpriteHelper = {
 };
 
 SpriteHelper.reset = function() {
-	var global = SpriteHelper;
-  global.view = {};
-  global.target = {};
-  global.zoom = 1;
-  global.update();
+	var g = SpriteHelper,
+      image = g.image;
+  g.zoom = 1;
+  g.scaled = { width: image.width, height: image.height };
+  g.focus = { x: image.width / 2, y: image.height / 2 };
+  g.paint();
 };
  
-SpriteHelper.update = function() {
-	var global = SpriteHelper,
-      canvas = global.canvas,
+SpriteHelper.paint = function() {
+	var g = SpriteHelper,
+      canvas = g.canvas,
       context = canvas.context,
-      image = global.image,
-      view = global.view,
-      target = global.target,
-      zoom = global.zoom,
-      virtual = { width: zoom * image.width, height: zoom * image.height };
-  // Width
-  view.width = Math.min(virtual.width, canvas.width);
-  view.x = (virtual.width - view.width) / 2;
-  target.x = Math.max(0, (canvas.width - view.width) / 2);
-  // Height
-  view.height = Math.min(virtual.height, canvas.height);
-  view.y = (virtual.height - view.height) / 2;
-  target.y = Math.max(0, (canvas.height - view.height) / 2);
-  // Paint background
+      image = g.image,
+      zoom = g.zoom,
+      scaled = g.scaled,
+      focus = g.focus,
+      view = {},
+      target = {};
+
+  // Compute the view dimensions within the scaled image, adjust the focus
+  //  if necessary to keep the view rectangle within the image, and
+  //  calculate the target rectangle on the canvas.
+  if (canvas.width >= scaled.width) {
+    view.width = scaled.width;
+    view.x = 0;
+    target.x = (canvas.width - view.width) / 2;
+    target.width = view.width;
+  } else {
+    view.width = canvas.width;
+    focus.x = Math.max(view.width/2, focus.x);
+    focus.x = Math.min(focus.x, scaled.width - view.width/2);
+    view.x = focus.x - view.width/2;
+    target.x = 0;
+    target.width = canvas.width;
+  }
+  if (canvas.height >= scaled.height) {
+    view.height = scaled.height;
+    view.y = 0;
+    target.y = (canvas.height - view.height) / 2;
+    target.height = view.height;
+  } else {
+    view.height = canvas.height;
+    focus.y = Math.max(view.height/2, focus.y);
+    focus.y = Math.min(focus.y, scaled.height - view.height/2);
+    view.y = focus.y - view.height/2;
+    target.y = 0;
+    target.height = canvas.height;
+  }
+
+  // Wipe the slate clean.
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = '#bbb';
+  context.fillStyle = '#fff';
   context.fillRect(target.x, target.y, view.width, view.height);
+
+  // Disable fuzzy interpolation.
   canvas.context.mozImageSmoothingEnabled = false;
   canvas.context.webkitImageSmoothingEnabled = false;
   canvas.context.msImageSmoothingEnabled = false;
   canvas.context.imageSmoothingEnabled = false;
-  context.drawImage(global.image,
-      view.x/zoom, view.y/zoom, view.width/zoom, view.height/zoom,
-      target.x, target.y, view.width, view.height);
+
+  // Scale the view rectangle back to the native image and copy it to
+  //   the target rectangle on the canvas.
+  context.drawImage(g.image,
+      view.x/g.zoom, view.y/g.zoom, view.width/g.zoom, view.height/g.zoom,
+      target.x, target.y, target.width, target.height);
 };
 
-SpriteHelper.zoomIn = function () {
-	var global = SpriteHelper;
-  if (global.zoom >= 1) {
-    global.zoom += 1;
+SpriteHelper.zoomBy = function (delta) {
+	var g = SpriteHelper,
+      image = g.image,
+      oldZoom = g.zoom,
+      newZoom,
+      scaled = g.scaled,
+      focus = g.focus;
+  if ((delta >= 0 && oldZoom >= 1) || (delta < 0 && oldZoom > 1)) {
+    newZoom = oldZoom + delta;
   } else {
-    global.zoom = 1/(Math.round(1/global.zoom) - 1);
+    newZoom = 1/(Math.round(1/oldZoom) - delta);
   }
-  global.update();
+  scaled.width = newZoom * image.width;
+  scaled.height = newZoom * image.height;
+  focus.x = newZoom * focus.x / oldZoom;
+  focus.y = newZoom * focus.y / oldZoom;
+  g.zoom = newZoom;
+  g.paint();
+};
+SpriteHelper.zoomIn = function () {
+  SpriteHelper.zoomBy(1);
 };
 SpriteHelper.zoomOut = function () {
-	var global = SpriteHelper;
-  if (global.zoom > 1) {
-    global.zoom -= 1;
-  } else {
-    global.zoom = 1/(Math.round(1/global.zoom) + 1);
-  }
-  global.update();
+  SpriteHelper.zoomBy(-1);
+};
+
+SpriteHelper.mouseDownCanvas = function (event) {
+	var g = SpriteHelper,
+      canvas = g.canvas,
+      focus = g.focus,
+      focusStart = { x: focus.x, y: focus.y },
+      mouseStart = { x: event.pageX, y: event.pageY };
+  //console.log('mouse down: '+event.pageX+', '+event.pageY);
+  //
+  var mouseMove = function (event) {
+    //console.log('mouse move: '+event.pageX+', '+event.pageY);
+    var x = event.pageX, y = event.pageY;
+    focus.x = focusStart.x + mouseStart.x - x;
+    focus.y = focusStart.y + mouseStart.y - y;
+    g.paint();
+  };
+  var mouseUp = function (event) {
+    //console.log('mouse up: '+event.pageX+', '+event.pageY);
+    $(window).off('mousemove', mouseMove);
+    $(window).off('mouseup', mouseUp);
+  };
+  $(window).mousemove(mouseMove);
+  $(window).mouseup(mouseUp);
 };
 
 SpriteHelper.load = function () {
-	var global = SpriteHelper;
+	var g = SpriteHelper;
 
-	var canvas = global.canvas = document.getElementById('mainCanvas');
+	var canvas = g.canvas = document.getElementById('mainCanvas');
   canvas.context = canvas.getContext('2d');
   canvas.width = $(window).width();
   canvas.height = $(window).height();
 
-  global.image = new Image();
-  global.image.src = global.imageSource;
-  global.image.onload = function () {
-    global.reset();
+  $(canvas).mousedown(g.mouseDownCanvas);
+
+  g.image = new Image();
+  g.image.src = g.imageSource;
+  g.image.onload = function () {
+    g.reset();
     $(window).on('resize', function () {
       canvas.width = $(window).width();
       canvas.height = $(window).height();
-      global.update();
+      g.paint();
     });
   };
 
   $(window).keydown(function (event) {
     var keyDownHandlers = {
-      68: global.zoomOut,
-      70: global.zoomIn
+      68: g.zoomOut,
+      70: g.zoomIn
     };
     var handler = keyDownHandlers[event.which];
     if (handler === undefined) {
