b392ecfaa799fd00e906baafab788625dedb2b80
Michael Laszlo <git@michaellaszlo.com>
2014-12-15 09:24:37 -0500

    Correct and simplify zooming code

--- js/sprite.helper.js
+++ js/sprite.helper.js
@@ -12,7 +12,6 @@ SpriteHelper.reset = function () {
 	var g = SpriteHelper,
       image = g.image;
   g.zoom = 1;
-  g.scaled = { width: image.width, height: image.height };
   g.focus = { x: image.width / 2, y: image.height / 2 };
   g.paint();
 };
@@ -33,7 +32,6 @@ SpriteHelper.paint = function() {
       context = canvas.context,
       image = g.image,
       zoom = g.zoom,
-      scaled = g.scaled,
       focus = g.focus,
       crop = {},
       target = {};
@@ -42,32 +40,33 @@ SpriteHelper.paint = function() {
   //  the target rectangle on the canvas. The focus is the center of the
   //  view of the scaled image. The span is the distance from the focus
   //  to the edge of the view.
-  var border = g.zoom * g.option.pan.minimumBorderPixels,
+  var border = g.option.pan.minimumBorderPixels,
     span = { x: canvas.width / g.zoom / 2,
              y: canvas.height / g.zoom / 2 };
   if (g.option.pan.free) {
     focus.x = g.between(border - span.x, focus.x,
-                        scaled.width + span.x - border);
+                        image.width + span.x - border);
   } else {
-    focus.x = g.between(Math.min(span.x, scaled.width/2), focus.x,
-                        Math.max(scaled.width - span.x, scaled.width/2));
+    focus.x = g.between(Math.min(span.x, image.width/2), focus.x,
+                        Math.max(image.width - span.x, image.width/2));
   }
-  crop.x = g.between(0, focus.x - span.x, scaled.width);
-  crop.width = g.between(0, focus.x + span.x, scaled.width) - crop.x;
+  crop.x = g.between(0, focus.x - span.x, image.width);
+  crop.width = g.between(0, focus.x + span.x, image.width) - crop.x;
   target.x = (crop.x - focus.x + span.x) * g.zoom;
   target.width = crop.width * g.zoom;
   if (g.option.pan.free) {
     focus.y = g.between(border - span.y, focus.y,
-                        scaled.height + span.y - border);
+                        image.height + span.y - border);
   } else {
-    focus.y = g.between(Math.min(span.y, scaled.height/2), focus.y,
-                        Math.max(scaled.height - span.y, scaled.height/2));
+    focus.y = g.between(Math.min(span.y, image.height/2), focus.y,
+                        Math.max(image.height - span.y, image.height/2));
   }
-  crop.y = g.between(0, focus.y - span.y, scaled.height);
-  crop.height = g.between(0, focus.y + span.y, scaled.height) - crop.y;
+  crop.y = g.between(0, focus.y - span.y, image.height);
+  crop.height = g.between(0, focus.y + span.y, image.height) - crop.y;
   target.y = (crop.y - focus.y + span.y) * g.zoom;
   target.height = crop.height * g.zoom;
   //console.log('focus: '+JSON.stringify(focus));
+  //console.log('span: '+JSON.stringify(span));
   //console.log('crop: '+JSON.stringify(crop));
   //console.log('target: '+JSON.stringify(target));
 
@@ -85,27 +84,18 @@ SpriteHelper.paint = function() {
   // Scale the crop rectangle back to the native image and copy it to
   //   the target rectangle on the canvas.
   context.drawImage(g.image,
-      crop.x/g.zoom, crop.y/g.zoom, crop.width/g.zoom, crop.height/g.zoom,
+      crop.x, crop.y, crop.width, crop.height,
       target.x, target.y, target.width, target.height);
 };
 
 SpriteHelper.zoomBy = function (delta) {
 	var g = SpriteHelper,
-      image = g.image,
-      oldZoom = g.zoom,
-      newZoom,
-      scaled = g.scaled,
-      focus = g.focus;
-  if ((delta >= 0 && oldZoom >= 1) || (delta < 0 && oldZoom > 1)) {
-    newZoom = oldZoom + delta;
+      zoom = g.zoom;
+  if ((delta >= 0 && zoom >= 1) || (delta < 0 && zoom > 1)) {
+    g.zoom = zoom + delta;
   } else {
-    newZoom = 1/(Math.round(1/oldZoom) - delta);
+    g.zoom = 1/(Math.round(1/zoom) - delta);
   }
-  scaled.width = newZoom * image.width;
-  scaled.height = newZoom * image.height;
-  focus.x = newZoom * focus.x / oldZoom;
-  focus.y = newZoom * focus.y / oldZoom;
-  g.zoom = newZoom;
   g.paint();
 };
 SpriteHelper.zoomIn = function () {
@@ -126,7 +116,7 @@ SpriteHelper.mouseDownCanvas = function (event) {
     //console.log('mouse move: '+event.pageX+', '+event.pageY);
     var dx = (mouseStart.x - event.pageX),
         dy = (mouseStart.y - event.pageY);
-    if (!g.option.pan.scaled) {
+    if (g.zoom < 1 || !g.option.pan.scaled) {
       dx /= g.zoom;
       dy /= g.zoom;
     }
